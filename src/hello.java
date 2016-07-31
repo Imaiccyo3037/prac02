e47719a28f5f97a31bdc951c0aa548f792f2abbf
yanchimo <bciyanmath@gmail.com>
2016-07-31 17:57:49 +0900

    ひろしの年齢をランダムで表示する関数を追加

--- src/hello.java
+++ src/hello.java
@@ -4,4 +4,13 @@ public class hello {
     System.out.println("Hello");
     System.out.println("Hello World!!");
   }
+  
+  /** 
+   * ひろしの年齢をランダムで表示
+   * @param max ひろしのmaxの年齢
+  */
+  public void printRandomHiroshiAge(int max){
+	  int r = (int)(Math.random() * max) + 1;
+	  System.out.println("ひろしは" + r + "歳です");
+  }
 }
